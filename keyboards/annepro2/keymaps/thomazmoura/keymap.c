c0dfb948ec6db0e40bdd1701ee230275dca290a5
Thomaz Moura <padilha.thomaz@gmail.com>
2020-08-28 23:33:23 -0300

    Fix arrows and add Win key on Right alt tap

--- keyboards/annepro2/keymaps/thomazmoura/keymap.c
+++ keyboards/annepro2/keymaps/thomazmoura/keymap.c
@@ -40,12 +40,12 @@ enum anne_pro_layers {
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
     LT(_FN1_LAYER,KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-    KC_LCTL, LGUI, KC_LALT, LT(_FN2_LAYER, KC_SPC), KC_RALT, LT(_FN2_LAYER,KC_APP), _FN1_LAYER, KC_RCTL
+    KC_LCTL, KC_LGUI, KC_LALT, LT(_FN2_LAYER, KC_SPC), LT(KC_RALT, KC_LGUI), LT(_FN2_LAYER,KC_APP), _FN1_LAYER, KC_RCTL
 ),
   /*
   * Layer _FN1_LAYER
   * ,-----------------------------------------------------------------------------------------.
-  * | ESC |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |  DELETE   |
+  * | Caps|  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |  DELETE   |
   * |-----------------------------------------------------------------------------------------+
   * | Tab    |  q  |  w  |  e  |  r  |  t  |  y  |  u  |  i  |  o  |  p  |  [  |  ]  |   \    |
   * |-----------------------------------------------------------------------------------------+
@@ -59,23 +59,23 @@ enum anne_pro_layers {
   */
  [_FN1_LAYER] = KEYMAP( /* Base */
     KC_CAPS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
-    KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_HOME, KC_END, KC_TRNS,
-    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_LEFT, KC_TRNS, KC_PGUP, KC_PGDN, KC_TRNS,
-    KC_TRNS, KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_DEL, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_LEFT, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_FN2_LAYER), MO(_FN1_LAYER), KC_TRNS
 ),
   /*
   * Layer _FN2_LAYER
   * ,-----------------------------------------------------------------------------------------.
-  * |  `  | BT1 | BT2 | BT3 | BT4 |  F5 |  F6 |  F7 |LEDOF|LEDON| F10 | F11 | F12 |    Bksp   |
+  * |  `  | BT1 | BT2 | BT3 | BT4 |  F5 |  F6 |  F7 |LEDOF|LEDON| F10 | F11 | F12 |           |
   * |-----------------------------------------------------------------------------------------+
-  * | Tab    |  q  |  w  |  e  |  r  |  t  | MUTE| PREV| PLAY| NEXT|  p  |  [  |  ]  |   \    |
+  * |        |     |     |     |     |     | MUTE| PREV| PLAY| NEXT|     |     |     |        |
   * |-----------------------------------------------------------------------------------------+
-  * | ESC     |  a  |  s  |  d  |  f  |  g  | HOME|PGDWN| PGUP| END |  ;  |  '  |    Enter    |
+  * |         |     |     |     |     |     | HOME|PGDWN| PGUP| END |     |     |             |
   * |-----------------------------------------------------------------------------------------+
-  * | Shift      |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  ,  |INSRT| DEL |    Shift       |
+  * |            |     |     |     |     |     |     |     |     |     |     |                |
   * |-----------------------------------------------------------------------------------------+
-  * | Ctrl  |  FN2  |  Alt  |               Space             |  Win  |  FN2  |  FN1  | Ctrl  |
+  * |       |  FN2  |       |               Space             |       |  FN2  |  FN1  | Ctrl  |
   * \-----------------------------------------------------------------------------------------/
   *
   */
