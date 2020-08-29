6b07950111d02b542d58d15c1de2413da776b82f
Thomaz Moura <padilha.thomaz@gmail.com>
2020-08-29 03:34:06 -0300

    First try on numpad layer

--- keyboards/annepro2/keymaps/thomazmoura/keymap.c
+++ keyboards/annepro2/keymaps/thomazmoura/keymap.c
@@ -6,6 +6,7 @@ enum anne_pro_layers {
   _BASE_LAYER,
   _FN1_LAYER,
   _FN2_LAYER,
+  _FN3_LAYER,
 };
 
 /*
@@ -40,7 +41,7 @@ enum anne_pro_layers {
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
     LT(_FN1_LAYER,KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-    KC_LCTL, KC_LGUI, KC_LALT, LT(_FN2_LAYER, KC_SPC), LT(KC_RALT, KC_LGUI), LT(_FN2_LAYER,KC_APP), _FN1_LAYER, KC_RCTL
+    KC_LCTL, _FN3_LAYER, KC_LALT, LT(_FN2_LAYER, KC_SPC), KC_LGUI, LT(_FN2_LAYER,KC_APP), _FN1_LAYER, KC_RCTL
 ),
   /*
   * Layer _FN1_LAYER
@@ -85,6 +86,28 @@ enum anne_pro_layers {
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+ ),
+  /*
+  * Layer _FN3_LAYER
+  * ,-----------------------------------------------------------------------------------------.
+  * |  `  | BT1 | BT2 | BT3 | BT4 |  F5 |  F6 |  F7 |LEDOF|LEDON| F10 | F11 | F12 |           |
+  * |-----------------------------------------------------------------------------------------+
+  * |        |     |     |     |     |     | MUTE| PREV| PLAY| NEXT|     |     |     |        |
+  * |-----------------------------------------------------------------------------------------+
+  * |         |     |     |     |     |     | HOME|PGDWN| PGUP| END |     |     |             |
+  * |-----------------------------------------------------------------------------------------+
+  * |            |     |     |     |     |     |VOLDN|VOLUP|     |     |     |                |
+  * |-----------------------------------------------------------------------------------------+
+  * |       |  FN2  |       |               Space             |       |  FN2  |  FN1  | Ctrl  |
+  * \-----------------------------------------------------------------------------------------/
+  *
+  */
+ [_FN3_LAYER] = KEYMAP( /* Base */
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_7, KC_8, KC_9, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_4, KC_5, KC_6, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_1, KC_2, KC_3, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_0, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
  ),
 };
 const uint16_t keymaps_size = sizeof(keymaps);
