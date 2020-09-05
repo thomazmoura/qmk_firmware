90da4002ecbf187ba950c360f7c0281bc743bea3
Thomaz Moura <padilha.thomaz@gmail.com>
2020-09-04 21:43:48 -0300

    Add ESC tap dance

--- keyboards/annepro2/keymaps/thomazmoura/keymap.c
+++ keyboards/annepro2/keymaps/thomazmoura/keymap.c
@@ -11,6 +11,23 @@ enum anne_pro_layers {
   _MOUSE_LAYER,
 };
 
+typedef struct {
+    bool is_press_action;
+    uint8_t state;
+} tap;
+
+// Define a type for as many tap dance states as you need
+enum {
+    SINGLE_TAP = 1,
+    SINGLE_HOLD,
+    DOUBLE_TAP,
+    DOUBLE_HOLD
+};
+
+enum {
+    ESC_LAYR, // Our custom tap dance key; add any other tap dance keys to this enum 
+};
+
 /*
 * Layer _BASE_LAYER
 * ,-----------------------------------------------------------------------------------------.
@@ -41,7 +58,7 @@ enum anne_pro_layers {
  [_BASE_LAYER] = KEYMAP( /* Base */
      KC_GRV,                    KC_1,  KC_2,  KC_3,  KC_4,  KC_5,   KC_6,      KC_7,    KC_8,     KC_9,     KC_0, KC_MINS,  KC_EQL, KC_BSPC,
     LT(_MOUSE_LAYER,KC_TAB),    KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,   KC_Y,      KC_U,    KC_I,     KC_O,     KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-    LT(_FUNCTION_LAYER,KC_ESC), KC_A,  KC_S,  KC_D,  KC_F,  KC_G,   KC_H,      KC_J,    KC_K,     KC_L,  KC_SCLN, KC_QUOT,  KC_ENT,
+    TD(ESC_LAYR), KC_A,  KC_S,  KC_D,  KC_F,  KC_G,   KC_H,      KC_J,    KC_K,     KC_L,  KC_SCLN, KC_QUOT,  KC_ENT,
     KC_LSFT, KC_Z,              KC_X,  KC_C,  KC_V,  KC_B,  KC_N,   KC_M,   KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,
     KC_LCTL,     TT(_NUMPAD_LAYER), KC_LALT, LT(_MEDIA_AND_NAVIGATION_LAYER, KC_SPC), KC_LGUI, LT(_MEDIA_AND_NAVIGATION_LAYER,KC_APP), MO(_FUNCTION_LAYER), KC_RCTL
 ),
@@ -147,3 +164,71 @@ void matrix_scan_user(void) {
 layer_state_t layer_state_set_user(layer_state_t layer) {
     return layer;
 }
+
+// Declare the functions to be used with your tap dance key(s)
+
+// Function associated with all tap dances
+uint8_t cur_dance(qk_tap_dance_state_t *state);
+
+// Functions associated with individual tap dances
+void ql_finished(qk_tap_dance_state_t *state, void *user_data);
+void ql_reset(qk_tap_dance_state_t *state, void *user_data);
+
+// Determine the current tap dance state
+uint8_t cur_dance(qk_tap_dance_state_t *state) {
+  if (state->count == 1) {
+    if (!state->pressed) return SINGLE_TAP;
+    else return SINGLE_HOLD;
+  } else if (state->count == 2) {
+    if (!state->pressed) return DOUBLE_TAP;
+    else return DOUBLE_HOLD;
+  }  else return 8;
+}
+
+// Initialize tap structure associated with example tap dance key
+static tap ql_tap_state = {
+  .is_press_action = true,
+  .state = 0
+};
+
+// Functions that control what our tap dance key does
+void ql_finished(qk_tap_dance_state_t *state, void *user_data) {
+  ql_tap_state.state = cur_dance(state);
+  switch (ql_tap_state.state) {
+    case SINGLE_TAP:
+      tap_code(KC_ESC);
+      break;
+    case SINGLE_HOLD:
+      layer_on(_FUNCTION_LAYER);
+      break;
+    case DOUBLE_TAP:
+      // Check to see if the layer is already set
+      if (layer_state_is(_MOUSE_LAYER)) {
+        // If already set, then switch it off
+        layer_off(_MOUSE_LAYER);
+      } else {
+        // If not already set, then switch the layer on
+        layer_on(_MOUSE_LAYER);
+      }
+      break;
+    case DOUBLE_HOLD:
+      layer_on(_NUMPAD_LAYER);
+      break;
+  }
+}
+
+void ql_reset(qk_tap_dance_state_t *state, void *user_data) {
+  // If the key was held down and now is released then switch off the layer
+  if (ql_tap_state.state == SINGLE_HOLD) {
+    layer_off(_FUNCTION_LAYER);
+  }
+  if (ql_tap_state.state == DOUBLE_HOLD) {
+    layer_off(_NUMPAD_LAYER);
+  }
+  ql_tap_state.state = 0;
+}
+
+// Associate our tap dance key with its functionality
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [ESC_LAYR] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, ql_finished, ql_reset, 275)
+};
