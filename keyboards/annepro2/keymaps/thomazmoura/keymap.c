fc56570822865cfbad8afe47c972eb33620a1f89
Thomaz Moura <padilha.thomaz@gmail.com>
2021-01-25 23:03:56 -0300

    Add sample layer and adjustments to media layer

--- keyboards/annepro2/keymaps/thomazmoura/keymap.c
+++ keyboards/annepro2/keymaps/thomazmoura/keymap.c
@@ -9,6 +9,7 @@ enum anne_pro_layers {
   _FUNCTION_LAYER,
   _MEDIA_AND_NAVIGATION_LAYER,
   _NUMPAD_LAYER,
+  _SAMPLE_LAYER,
 };
 
 typedef struct {
@@ -23,7 +24,8 @@ enum {
     DOUBLE_TAP,
     DOUBLE_HOLD,
     TRIPLE_TAP,
-    TRIPLE_HOLD
+    TRIPLE_HOLD,
+    QUAD_TAP
 };
 
 enum profile {
@@ -61,126 +63,49 @@ enum {
     GRV_TAP_DANCE,
 };
 
-/*
-* Layer _BASE_LAYER
-* ,-----------------------------------------------------------------------------------------.
-* |  `  |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |    Bksp   |
-* |-----------------------------------------------------------------------------------------+
-* | Tab    |  q  |  w  |  e  |  r  |  t  |  y  |  u  |  i  |  o  |  p  |  [  |  ]  |   \    |
-* |-----------------------------------------------------------------------------------------+
-* |-----------------------------------------------------------------------------------------+ | Shift      |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  ,  |  .  |  /  |    Shift       |
-* |-----------------------------------------------------------------------------------------+
-* | Ctrl  |  FN2  |  Alt  |                FN2              |  Win  |  FN2  |  FN1  | Ctrl  |
-* \-----------------------------------------------------------------------------------------/
-* Layer TAP in _BASE_LAYER
-* ,-----------------------------------------------------------------------------------------.
-* |     |     |     |     |     |     |     |     |     |     |     |     |     |           |
-* |-----------------------------------------------------------------------------------------+
-* |        |     |     |     |     |     |     |     |     |     |     |     |     |        |
-* |-----------------------------------------------------------------------------------------+
-* | ESC     |     |     |     |     |     |     |     |     |     |     |     |             |
-* |-----------------------------------------------------------------------------------------+
-* |            |     |     |     |     |     |     |     |     |     |     |                |
-* |-----------------------------------------------------------------------------------------+
-* |       |       |       |               Space             |       |       |       |       |
-* \-----------------------------------------------------------------------------------------/
-*/
- const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
- [_BASE_LAYER] = KEYMAP( /* Base */
+const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE_LAYER] = KEYMAP(
             KC_GRV,    KC_1,    KC_2,  KC_3,  KC_4,  KC_5,      KC_6,    KC_7,    KC_8,           KC_9,     KC_0,   KC_MINS,  KC_EQL, KC_BSPC,
             KC_TAB,    KC_Q,    KC_W,  KC_E,  KC_R,  KC_T,      KC_Y,    KC_U,    KC_I,           KC_O,     KC_P,   KC_LBRC, KC_RBRC, KC_BSLS,
- TD(ESC_TAP_DANCE),    KC_A,    KC_S,  KC_D,  KC_F,  KC_G,      KC_H,    KC_J,    KC_K,           KC_L,  KC_SCLN,   KC_QUOT,  KC_ENT,
+  TD(ESC_TAP_DANCE),    KC_A,    KC_S,  KC_D,  KC_F,  KC_G,      KC_H,    KC_J,    KC_K,           KC_L,  KC_SCLN,   KC_QUOT,  KC_ENT,
            KC_LSFT,    KC_Z,    KC_X,  KC_C,  KC_V,  KC_B,      KC_N,    KC_M, KC_COMM,         KC_DOT,  KC_SLSH,   KC_RSFT,
            KC_LCTL, KC_LGUI, KC_LALT, LT(_MEDIA_AND_NAVIGATION_LAYER, KC_SPC), LALT_T(KC_APP), KC_RGUI, MO(_FUNCTION_LAYER), KC_RCTL
-),
-  /*
-  * Layer _FUNCTION_LAYER
-  * ,-----------------------------------------------------------------------------------------.
-  * | Caps|  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |  DELETE   |
-  * |-----------------------------------------------------------------------------------------+
-  * |        |     |  7  |  8  |  9  |     |     |     |     |     |     |     |     |        |
-  * |-----------------------------------------------------------------------------------------+
-  * |         |  0  |  4  |  5  |  6  |     | LFT | DWN | UP  | RGT |  ;  |  '  |             |
-  * |-----------------------------------------------------------------------------------------+
-  * |            |     |  1  |  2  |  3  |     |     |     |     |     |     |                |
-  * |-----------------------------------------------------------------------------------------+
-  * |       |       |       |                                 |       |       |       |       |
-  * \-----------------------------------------------------------------------------------------/
-  *
-  */
- [_FUNCTION_LAYER] = KEYMAP( /* Base */
+  ),
+  [_FUNCTION_LAYER] = KEYMAP(
     KC_CAPS,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_DEL,
     KC_PSCR, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______
-),
-  /*
-  * Layer _MEDIA_AND_NAVIGATION_LAYER
-  * ,-----------------------------------------------------------------------------------------.
-  * | USB | BT1 | BT2 | BT3 | BT4 |     |     |     |     |LEDCH|LEDON|LEDIN|LEDSP|           |
-  * |-----------------------------------------------------------------------------------------+
-  * |        |     |     |     |     |     | MUTE| PREV| PLAY| NEXT|     |     |     |        |
-  * |-----------------------------------------------------------------------------------------+
-  * |         |     |     |     |     |     | HOME|PGDWN| PGUP| END |     |     |             |
-  * |-----------------------------------------------------------------------------------------+
-  * |            |     |     |     |     |     |     |     |VOLDN|VOLUP|     |                |
-  * |-----------------------------------------------------------------------------------------+
-  * |       |       |       |                                 |       |       |       |       |
-  * \-----------------------------------------------------------------------------------------/
-  *
-  */
- [_MEDIA_AND_NAVIGATION_LAYER] = KEYMAP( /* Base */
-    KC_AP2_USB, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, _______, _______, _______, _______,  NEXT_PROFILE, ENABLE_OR_DISABLE_LEDS, KC_AP_LED_NEXT_INTENSITY, KC_AP_LED_SPEED,  KC_AP2_BT_UNPAIR,
-       _______,    _______,    _______,      KC_UP,    _______, _______, KC_MUTE, KC_MPRV, KC_MPLY,       KC_MNXT,                _______, KC_BRID, KC_BRIU, _______,
+  ),
+  [_MEDIA_AND_NAVIGATION_LAYER] = KEYMAP(
+    KC_AP2_USB, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, _______, _______, _______, _______,  NEXT_PROFILE, ENABLE_OR_DISABLE_LEDS, KC_AP_LED_NEXT_INTENSITY, KC_AP_LED_SPEED,  KC_PAUSE,
+       _______,    _______,    _______,      KC_UP,    _______, _______, KC_MUTE, KC_MPRV, KC_MPLY,       KC_MNXT,                _______, KC_BRID, KC_BRIU, KC_AP2_BT_UNPAIR,
        _______,    _______,    KC_LEFT,    KC_DOWN,    KC_RGHT, _______, KC_HOME, KC_PGDN, KC_PGUP,        KC_END,                _______, _______, _______,
        _______,    _______,    _______,    _______,    _______, _______, _______, _______, KC_VOLD,       KC_VOLU,                _______, _______,
        _______,    _______,    _______,    _______,    _______, _______, _______, _______
- ),
-  /*
-  * Layer _NUMPAD_LAYER
-  * ,-----------------------------------------------------------------------------------------.
-* |     |     |     |     |     |     |     |     |     |     |     |     |     |           |
-* |-----------------------------------------------------------------------------------------+
-* |        |     |  7  |  8  |  9  |     |  +  |  7  |  8  |  9  |  .  |     |     |        |
-* |-----------------------------------------------------------------------------------------+
-* |         |  0  |  4  |  5  |  6  |     |  0  |  4  |  5  |  6  |  /  |     |             |
-* |-----------------------------------------------------------------------------------------+
-* |            |     |  1  |  2  |  3  |  ,  |  *  |  1  |  2  |  3  |  -  |                |
-* |-----------------------------------------------------------------------------------------+
-* |       |       |       |               0                 |       |       |       |       |
-  * \-----------------------------------------------------------------------------------------/
-  *
-  */
- [_NUMPAD_LAYER] = KEYMAP( /* Base */
+  ),
+  [_NUMPAD_LAYER] = KEYMAP(
     _______, _______, _______, _______, _______, _______, _______, _______, KC_PAST, _______,    KC_PSLS,  KC_PMNS, KC_PPLS, _______,
     _______, _______, _______,   KC_UP, _______, _______, _______,    KC_7,    KC_8,    KC_9,    KC_COMM,  _______, _______, _______,
     _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,    KC_0,    KC_4,    KC_5,    KC_6, S(KC_SCLN),   KC_DOT, _______,
     _______, _______, _______, _______, _______, _______, _______,    KC_1,    KC_2,    KC_3,    KC_SLSH,  _______,
     _______, _______, _______,    KC_0, _______, _______, _______, _______
- ),
-  /*
-  * Layer _MOUSE_LAYER - Mouse Layer
-  * ,-----------------------------------------------------------------------------------------.
-  * |     |     |     |     |     |     |     |     |     |     |     |     |     |           |
-  * |-----------------------------------------------------------------------------------------+
-  * |        |     |     |     |     |     |ACL2 |WL-U |MS-U |WL-D |ACL1 |ACL0 |     |        |
-  * |-----------------------------------------------------------------------------------------+
-  * |         |     | ACL0 | ACL1 | ACL2 |     |BTN3 |MS-L |MS-D |MS-R |BTN2 |     |             |
-  * |-----------------------------------------------------------------------------------------+
-  * |            |     |     |     |     |     |BTN4 |BTN5 |WL-L |WL-R |     |                |
-  * |-----------------------------------------------------------------------------------------+
-  * |       |       |       |              BTN1               |       |       |       |       |
-  * \-----------------------------------------------------------------------------------------/
-  *
-  */
- [_MOUSE_LAYER] = KEYMAP( /* Base */
+  ),
+  [_MOUSE_LAYER] = KEYMAP(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______, _______, _______,
     _______, _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN2, _______, _______,
     _______, _______, _______, _______, _______, _______, KC_BTN3, KC_BTN4, KC_BTN5, _______, _______, _______, 
     _______, _______, _______, KC_BTN1, _______, _______, _______, _______
- ),
+  ),
+  [_SAMPLE_LAYER] = KEYMAP(
+    KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
+    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
+    KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP),
+    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, LT(_MEDIA_AND_NAVIGATION_LAYER,KC_LEFT), LT(_FUNCTION_LAYER,KC_DOWN), RCTL_T(KC_RGHT)
+  ),
 };
 const uint16_t keymaps_size = sizeof(keymaps);
 
@@ -206,6 +131,7 @@ uint8_t function_profile[] = {0x00,0xFF,0x00};
 uint8_t navigation_profile[] = {0x44,0x00,0xFF};
 uint8_t numpad_profile[] = {0xFF,0xDD,0x00};
 uint8_t mouse_profile[] = {0x00,0x88,0xFF};
+uint8_t sample_profile[] = {0xDD,0x44,0x00};
 
 void matrix_init_user(void) {
 }
@@ -252,6 +178,9 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     case _MOUSE_LAYER:
       enableProfileColor(mouse_profile);
       break;
+    case _SAMPLE_LAYER:
+      enableProfileColor(sample_profile);
+      break;
     case _MEDIA_AND_NAVIGATION_LAYER:
       enableProfileColor(navigation_profile);
       break;
@@ -274,7 +203,10 @@ uint8_t cur_dance(qk_tap_dance_state_t *state) {
   } else if (state->count == 3) {
     if (!state->pressed) return TRIPLE_TAP;
     else return TRIPLE_HOLD;
-  }  else return 8;
+  } else if (state->count == 4) {
+    if (!state->pressed) return QUAD_TAP;
+    else return 8;
+  } else return 8;
 }
 
 // Initialize tap structure associated with example tap dance key
@@ -369,6 +301,13 @@ void esc_layer_finished(qk_tap_dance_state_t *state, void *user_data) {
       break;
     case TRIPLE_HOLD:
       layer_on(_MOUSE_LAYER);
+      break;
+    case QUAD_TAP:
+      if (layer_state_is(_SAMPLE_LAYER)) {
+        layer_off(_SAMPLE_LAYER);
+      } else {
+        layer_on(_SAMPLE_LAYER);
+      }
       break;
   }
 }
