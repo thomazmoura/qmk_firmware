52e8a3005cec1926f1c658def0f5e8e91c7f3a9e
Thomaz Moura <padilha.thomaz@gmail.com>
2021-02-07 23:55:27 -0300

    Add some minor adjustments

--- keyboards/annepro2/keymaps/thomazmoura/keymap.c
+++ keyboards/annepro2/keymaps/thomazmoura/keymap.c
@@ -100,7 +100,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, KC_BTN1, _______, _______, _______, _______
   ),
   [_SAMPLE_LAYER] = KEYMAP(
-    KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
+    TD(ESC_TAP_DANCE), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
     KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP),
@@ -281,9 +281,10 @@ void esc_layer_finished(qk_tap_dance_state_t *state, void *user_data) {
       layer_on(_FUNCTION_LAYER);
       break;
     case DOUBLE_TAP:
-      if (layer_state_is(_MOUSE_LAYER) || layer_state_is(_NUMPAD_LAYER)) {
+      if (layer_state_is(_MOUSE_LAYER) || layer_state_is(_NUMPAD_LAYER) || layer_state_is(_MEDIA_AND_NAVIGATION_LAYER)) {
         layer_off(_MOUSE_LAYER);
         layer_off(_NUMPAD_LAYER);
+        layer_off(_SAMPLE_LAYER);
       } else {
         tap_code(KC_ESC);
         tap_code(KC_ESC);
