04fa70f21f46708f8dfb60b9afa1990ee02f90c6
Thomaz Moura <padilha.thomaz@gmail.com>
2022-08-28 19:37:16 -0300

    Adjustments to make it work with official QMK

--- keyboards/annepro2/keymaps/thomazmoura/keymap.c
+++ keyboards/annepro2/keymaps/thomazmoura/keymap.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 #include "annepro2.h"
-#include "qmk_ap2_led.h"
+#include "ap2_led.h"
 #include "config.h"
 
 enum anne_pro_layers {
@@ -72,35 +72,35 @@ enum {
 };
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE_LAYER] = KEYMAP(
+  [_BASE_LAYER] = LAYOUT_60_ansi(
             KC_GRV,    KC_1,    KC_2,  KC_3,  KC_4,  KC_5,      KC_6,    KC_7,    KC_8,           KC_9,     KC_0,   KC_MINS,  KC_EQL, KC_BSPC,
             KC_TAB,    KC_Q,    KC_W,  KC_E,  KC_R,  KC_T,      KC_Y,    KC_U,    KC_I,           KC_O,     KC_P,   KC_LBRC, KC_RBRC, KC_BSLS,
   TD(ESC_TAP_DANCE),    KC_A,    KC_S,  KC_D,  KC_F,  KC_G,      KC_H,    KC_J,    KC_K,           KC_L,  KC_SCLN,   KC_QUOT,  KC_ENT,
            KC_LSFT,    KC_Z,    KC_X,  KC_C,  KC_V,  KC_B,      KC_N,    KC_M, KC_COMM,         KC_DOT,  KC_SLSH,   KC_RSFT,
            KC_LCTL, KC_LGUI, KC_LALT, LT(_CORRECTION_LAYER, KC_SPC), LALT_T(KC_APP), KC_RGUI, MO(_FUNCTION_LAYER), KC_RCTL
   ),
-  [_FUNCTION_LAYER] = KEYMAP(
+  [_FUNCTION_LAYER] = LAYOUT_60_ansi(
     KC_CAPS,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_DEL,
     KC_PSCR, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, LT(_MEDIA_AND_NAVIGATION_LAYER, KC_SPC), _______, _______, TG(_GAME_LAYER), _______
   ),
-  [_NUMPAD_LAYER] = KEYMAP(
+  [_NUMPAD_LAYER] = LAYOUT_60_ansi(
     _______, _______, _______, _______, _______, _______, _______, _______, KC_PAST, _______,    KC_PSLS,  KC_PMNS, KC_PPLS, _______,
     _______, _______, _______,   KC_UP, _______, _______, _______,    KC_7,    KC_8,    KC_9,    KC_COMM,  _______, _______, _______,
     _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,    KC_0,    KC_4,    KC_5,    KC_6, S(KC_SCLN),   KC_DOT, _______,
     _______, _______, _______, _______, _______, _______, _______,    KC_1,    KC_2,    KC_3,    KC_SLSH,  _______,
     _______, _______, _______, LT(_CORRECTION_LAYER, KC_SPC), TG(_NUMPAD_LAYER), _______, _______, _______
   ),
-  [_CORRECTION_LAYER] = KEYMAP(
+  [_CORRECTION_LAYER] = LAYOUT_60_ansi(
     _______,                               _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PAUSE,
     _______,                               _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     LT(_MEDIA_AND_NAVIGATION_LAYER, KC_0), _______, _______, _______, _______, _______, KC_BSPC, _______, _______,  KC_DEL, _______, _______, _______,
     _______,                               _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
     _______,    _______,    _______,    _______, TG(_NUMPAD_LAYER), TG(_MOUSE_LAYER), _______, _______
   ),
-  [_MOUSE_LAYER] = KEYMAP(
+  [_MOUSE_LAYER] = LAYOUT_60_ansi(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______, _______, _______,
     _______, _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN2, _______, _______,
@@ -108,14 +108,14 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, KC_BTN1, _______, TG(_MOUSE_LAYER), _______, _______
 
   ),
-  [_MEDIA_AND_NAVIGATION_LAYER] = KEYMAP(
+  [_MEDIA_AND_NAVIGATION_LAYER] = LAYOUT_60_ansi(
     KC_AP2_USB, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, _______,        _______,      _______, _______, NEXT_PROFILE, ENABLE_OR_DISABLE_LEDS, KC_AP_LED_NEXT_INTENSITY, KC_AP_LED_SPEED,  KC_PAUSE,
        _______,    _______,    _______,      KC_UP,    _______, _______,        KC_MUTE,      KC_MPRV, KC_MPLY,      KC_MNXT,                _______, KC_BRID, KC_BRIU, KC_AP2_BT_UNPAIR,
        _______,    _______,    KC_LEFT,    KC_DOWN,    KC_RGHT, _______,        KC_HOME,      KC_PGDN, KC_PGUP,       KC_END,                _______, _______, _______,
        _______,    _______,    _______,    _______,    _______, _______,   MINIMIZE_APP, MAXIMIZE_APP, KC_VOLD,      KC_VOLU, _______, _______,
        _______,    _______,    _______,    _______, TG(_NUMPAD_LAYER), TG(_MOUSE_LAYER), TG(_GAME_LAYER), _______
   ),
-  [_GAME_LAYER] = KEYMAP(
+  [_GAME_LAYER] = LAYOUT_60_ansi(
                         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     LT(_FUNCTION_LAYER, KC_TAB), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, LT(_FUNCTION_LAYER, KC_BSLS),
                          KC_ESC, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -132,8 +132,8 @@ const uint16_t keymaps_size = sizeof(keymaps);
 uint8_t cur_dance(qk_tap_dance_state_t *state);
 
 // Functions associated with individual tap dances
-void enableProfileColor(uint8_t * profile);
-void resetProfileColor(void);
+void enable_profile_color(uint8_t * profile);
+void reset_profile_color(void);
 void esc_layer_finished(qk_tap_dance_state_t *state, void *user_data);
 void esc_layer_reset(qk_tap_dance_state_t *state, void *user_data);
 
@@ -153,32 +153,20 @@ uint8_t game_layer[] = {0x80,0xFF,0x99};;
 void matrix_init_user(void) {
 }
 
-void matrix_scan_user(void) {
-    while(!sdGetWouldBlock(&SD1)) {
-        sdReadTimeout(&SD1, (uint8_t *) &BLECapsLock, sizeof(ble_capslock_t), 10);
-
-        if (BLECapsLock.caps_lock) {
-            annepro2LedSetForegroundColor(0xFF, 0x00, 0x00);
-        } else {
-            annepro2LedResetForegroundColor();
-        }
-    }
-}
-
 void keyboard_post_init_user(void) {
-  annepro2LedEnable();
-  resetProfileColor();
+  ap2_led_enable();
+  reset_profile_color();
 }
 
 // The function to handle the caps lock logic
 bool led_update_user(led_t leds) {
   if (leds.caps_lock) {
     is_caps_set = true;
-    enableProfileColor(caps_profile);
+    enable_profile_color(caps_profile);
     return true;
   } else if(is_caps_set) {
     is_caps_set = false;
-    resetProfileColor();
+    reset_profile_color();
   }
 
   return true;
@@ -187,25 +175,25 @@ bool led_update_user(led_t leds) {
 layer_state_t layer_state_set_user(layer_state_t state) {
   switch(get_highest_layer(state)) {
     case _FUNCTION_LAYER:
-      enableProfileColor(function_profile);
+      enable_profile_color(function_profile);
       break;
     case _NUMPAD_LAYER:
-      enableProfileColor(numpad_profile);
+      enable_profile_color(numpad_profile);
       break;
     case _MOUSE_LAYER:
-      enableProfileColor(mouse_profile);
+      enable_profile_color(mouse_profile);
       break;
     case _MEDIA_AND_NAVIGATION_LAYER:
-      enableProfileColor(navigation_profile);
+      enable_profile_color(navigation_profile);
       break;
     case _CORRECTION_LAYER:
-      enableProfileColor(correction_profile);
+      enable_profile_color(correction_profile);
       break;
     case _GAME_LAYER:
-      enableProfileColor(game_layer);
+      enable_profile_color(game_layer);
       break;
     default:
-      resetProfileColor();
+      reset_profile_color();
       break;
   }
 
@@ -240,21 +228,21 @@ static tap grav_tap_state = {
   .state = 0
 };
 
-void enableProfileColor (uint8_t * profile) {
+void enable_profile_color (uint8_t * profile) {
   if(is_caps_set) {
-    annepro2LedSetForegroundColor(caps_profile[0], caps_profile[1], caps_profile[2]);
+    ap2_led_set_foreground_color(caps_profile[0], caps_profile[1], caps_profile[2]);
   } else {
-    annepro2LedSetForegroundColor(profile[0], profile[1], profile[2]);
+    ap2_led_set_foreground_color(profile[0], profile[1], profile[2]);
   }
 }
 
-void resetProfileColor(void) {
+void reset_profile_color(void) {
   if(is_caps_set) {
-    annepro2LedSetForegroundColor(caps_profile[0], caps_profile[1], caps_profile[2]);
+    ap2_led_set_foreground_color(caps_profile[0], caps_profile[1], caps_profile[2]);
   } else if(base_profile == IDLE_PROFILE_INDEX) {
-    annepro2LedSetForegroundColor(idle_profile[0], idle_profile[1], idle_profile[2]);
+    ap2_led_set_foreground_color(idle_profile[0], idle_profile[1], idle_profile[2]);
   } else {
-    annepro2LedResetForegroundColor();
+    ap2_led_reset_foreground_color();
   } 
 }
 
@@ -267,9 +255,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           base_profile = IDLE_PROFILE_INDEX;
 
         if(base_profile == IDLE_PROFILE_INDEX) {
-          annepro2LedSetForegroundColor(idle_profile[0], idle_profile[1], idle_profile[2]);
+          ap2_led_set_foreground_color(idle_profile[0], idle_profile[1], idle_profile[2]);
         } else {
-          annepro2LedSetProfile(cyclabe_profiles[base_profile]);
+          ap2_led_reset_foreground_color();
         }
       }
       return true;
@@ -277,9 +265,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         if(is_led_on) {
           is_led_on = false;
-          annepro2LedDisable();
+          ap2_led_disable();
         } else {
-          annepro2LedEnable();
+          ap2_led_enable();
           is_led_on = true;
         }
       }
@@ -394,8 +382,8 @@ void grave_layer_reset(qk_tap_dance_state_t *state, void *user_data) {
 
 // Associate our tap dance key with its functionality
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [ESC_TAP_DANCE] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, esc_layer_finished, esc_layer_reset, 250),
-  [GRV_TAP_DANCE] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, grave_layer_finished, grave_layer_reset, 300)
+  [ESC_TAP_DANCE] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, esc_layer_finished, esc_layer_reset),
+  [GRV_TAP_DANCE] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, grave_layer_finished, grave_layer_reset)
 };
 
 
