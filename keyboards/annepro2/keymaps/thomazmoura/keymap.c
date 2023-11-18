8fe5953a4eb7d3976f7dd91d4c7ad16ee7166f44
Thomaz Moura <thomaz.moura@autotrac.com.br>
2023-11-17 23:36:35 -0300

    Attempt at getting ideal tapping term per key for the home row mods.

--- keyboards/annepro2/keymaps/thomazmoura/keymap.c
+++ keyboards/annepro2/keymaps/thomazmoura/keymap.c
@@ -358,24 +358,28 @@ tap_dance_action_t tap_dance_actions[] = {
   [GRV_TAP_DANCE] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, grave_layer_finished, grave_layer_reset)
 };
 
+uint16_t ctrl_tapping_term = 200;
+uint16_t shift_tapping_term = 175;
+uint16_t alt_tapping_term = 175;
+uint16_t gui_tapping_term = 175;
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case MT(MOD_LCTL, KC_A):
-            return 150;
+            return ctrl_tapping_term;
         case MT(MOD_LSFT, KC_S):
-            return 150;
+            return shift_tapping_term;
         case MT(MOD_LGUI, KC_D):
-            return 150;
+            return gui_tapping_term;
         case MT(MOD_LALT, KC_F):
-            return 150;
+            return alt_tapping_term;
         case MT(MOD_LALT, KC_J):
-            return 150;
+            return alt_tapping_term;
         case MT(MOD_LGUI, KC_K):
-            return 150;
+            return gui_tapping_term;
         case MT(MOD_RSFT, KC_L):
-            return 150;
+            return shift_tapping_term;
         case MT(MOD_RCTL, KC_SCLN):
-            return 150;
+            return ctrl_tapping_term;
         default:
             return TAPPING_TERM;
     }
