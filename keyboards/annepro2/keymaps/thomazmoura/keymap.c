455cf39e493a25c05bcb17e653b2dd3bd31dd1b4
Thomaz Moura <thomaz.moura@autotrac.com.br>
2024-04-24 09:48:18 -0300

    Remove tap_dance and add combos for grave and tilt

--- keyboards/annepro2/keymaps/thomazmoura/keymap.c
+++ keyboards/annepro2/keymaps/thomazmoura/keymap.c
@@ -23,17 +23,6 @@ typedef struct {
     uint8_t state;
 } tap;
 
-// Define a type for as many tap dance states as you need
-enum {
-    SINGLE_TAP = 1,
-    SINGLE_HOLD,
-    DOUBLE_TAP,
-    DOUBLE_HOLD,
-    TRIPLE_TAP,
-    TRIPLE_HOLD,
-    QUAD_TAP,
-};
-
 enum custom_codes {
   AP2_SAFE_RANGE = KC_AP_RGB_MOD,
   KC_MAXIMIZE,
@@ -42,16 +31,11 @@ enum custom_codes {
   KC_RGB_NEXT,
 };
 
-enum {
-    ESC_TAP_DANCE,
-    GRV_TAP_DANCE,
-};
-
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE_LAYER] = LAYOUT_60_ansi(
             KC_GRV,    KC_1,    KC_2,  KC_3,  KC_4,  KC_5,      KC_6,    KC_7,    KC_8,           KC_9,     KC_0,   KC_MINS,  KC_EQL, KC_BSPC,
             KC_TAB,    KC_Q,    LT(_NUMPAD_LAYER, KC_W),  LT(_SYMBOLS_LAYER, KC_E), KC_R, KC_T, KC_Y, KC_U, LT(_SYMBOLS_LAYER, KC_I), LT(_NUMPAD_LAYER, KC_O), KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-  TD(ESC_TAP_DANCE),  MT(MOD_LCTL, KC_A), MT(MOD_LSFT, KC_S),  MT(MOD_LGUI, KC_D),  MT(MOD_LALT, KC_F),  KC_G,      KC_H,    MT(MOD_LALT, KC_J),    MT(MOD_LGUI, KC_K), MT(MOD_RSFT, KC_L),  MT(MOD_RCTL, KC_SCLN),   KC_QUOT,  KC_ENT,
+  LT(_FUNCTION_LAYER, KC_ESC),  MT(MOD_LCTL, KC_A), MT(MOD_LSFT, KC_S),  MT(MOD_LGUI, KC_D),  MT(MOD_LALT, KC_F),  KC_G,      KC_H,    MT(MOD_LALT, KC_J),    MT(MOD_LGUI, KC_K), MT(MOD_RSFT, KC_L),  MT(MOD_RCTL, KC_SCLN),   KC_QUOT,  KC_ENT,
            KC_LSFT,    LT(_FUNCTION_KEYS_LAYER, KC_Z),    LT(_BRACKETS_LAYER, KC_X),  KC_C,  KC_V,  KC_B,      KC_N,    KC_M, KC_COMM,         KC_DOT,  LT(_FUNCTION_KEYS_LAYER, KC_SLSH),   KC_RSFT,
            KC_LCTL, KC_LGUI, KC_LALT, LT(_CORRECTION_LAYER, KC_SPC), LALT_T(KC_APP), KC_RGUI, MO(_FUNCTION_LAYER), KC_RCTL
   ),
@@ -130,16 +114,8 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t keymaps_size = sizeof(keymaps);
 
 
-// Declare the functions to be used with your tap dance key(s)
-
-// Function associated with all tap dances
-uint8_t cur_dance(tap_dance_state_t *state);
-
-// Functions associated with individual tap dances
 void enable_profile_color(uint8_t * profile);
 void reset_profile_color(void);
-void esc_layer_finished(tap_dance_state_t *state, void *user_data);
-void esc_layer_reset(tap_dance_state_t *state, void *user_data);
 
 bool is_focus_mode_on = true;
 bool is_caps_on = false;
@@ -222,34 +198,6 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   return state;
 }
 
-// Determine the current tap dance state
-uint8_t cur_dance(tap_dance_state_t *state) {
-  if (state->count == 1) {
-    if (!state->pressed) return SINGLE_TAP;
-    else return SINGLE_HOLD;
-  } else if (state->count == 2) {
-    if (!state->pressed) return DOUBLE_TAP;
-    else return DOUBLE_HOLD;
-  } else if (state->count == 3) {
-    if (!state->pressed) return TRIPLE_TAP;
-    else return TRIPLE_HOLD;
-  } else if (state->count == 4) {
-    if (!state->pressed) return QUAD_TAP;
-    else return 8;
-  } else return 8;
-}
-
-// Initialize tap structure associated with example tap dance key
-static tap esc_tap_state = {
-  .is_press_action = true,
-  .state = 0
-};
-
-static tap grav_tap_state = {
-  .is_press_action = true,
-  .state = 0
-};
-
 void enable_profile_color (uint8_t * profile) {
   ap2_led_set_foreground_color(profile[0], profile[1], profile[2]);
 }
@@ -293,103 +241,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
 }
 
-// Functions that control what our tap dance key does
-void esc_layer_finished(tap_dance_state_t *state, void *user_data) {
-  esc_tap_state.state = cur_dance(state);
-
-  switch (esc_tap_state.state) {
-    case SINGLE_TAP:
-      tap_code(KC_ESC);
-      break;
-    case SINGLE_HOLD:
-      layer_on(_FUNCTION_LAYER);
-      break;
-    case DOUBLE_TAP:
-      if (layer_state_is(_MOUSE_LAYER) || layer_state_is(_NUMPAD_LAYER) || layer_state_is(_GAME_LAYER) ) {
-        layer_off(_MOUSE_LAYER);
-        layer_off(_NUMPAD_LAYER);
-        layer_off(_GAME_LAYER);
-      } else {
-        tap_code(KC_ESC);
-        tap_code(KC_ESC);
-      }
-      break;
-    case DOUBLE_HOLD:
-      layer_on(_NUMPAD_LAYER);
-      break;
-    case TRIPLE_TAP:
-      if (layer_state_is(_MOUSE_LAYER)) {
-        layer_off(_MOUSE_LAYER);
-      } else {
-        layer_on(_MOUSE_LAYER);
-      }
-      break;
-    case TRIPLE_HOLD:
-      layer_on(_MOUSE_LAYER);
-      break;
-    case QUAD_TAP:
-      if (layer_state_is(_NUMPAD_LAYER)) {
-        layer_off(_NUMPAD_LAYER);
-      } else {
-        layer_on(_NUMPAD_LAYER);
-      }
-      break;
-  }
-}
-
-void esc_layer_reset(tap_dance_state_t *state, void *user_data) {
-  // If the key was held down and now is released then switch off the layer
-  if (esc_tap_state.state == SINGLE_HOLD) {
-    layer_off(_FUNCTION_LAYER);
-  }
-  if (esc_tap_state.state == DOUBLE_HOLD) {
-    layer_off(_NUMPAD_LAYER);
-  }
-  if (esc_tap_state.state == TRIPLE_HOLD) {
-    layer_off(_MOUSE_LAYER);
-  }
-  esc_tap_state.state = 0;
-}
-
-// Functions that control what our tap dance key does
-void grave_layer_finished(tap_dance_state_t *state, void *user_data) {
-  grav_tap_state.state = cur_dance(state);
-
-  switch (grav_tap_state.state) {
-    case SINGLE_TAP:
-      tap_code(KC_GRV);
-      break;
-    case SINGLE_HOLD:
-      tap_code(KC_GRV);
-      break;
-    case DOUBLE_TAP:
-      // Check to see if the layer is already set
-      if (layer_state_is(_MOUSE_LAYER)) {
-        // If already set, then switch it off
-        layer_off(_MOUSE_LAYER);
-      } else {
-        // If not already set, then switch the layer on
-        layer_on(_MOUSE_LAYER);
-      }
-      break;
-  }
-}
-
-void grave_layer_reset(tap_dance_state_t *state, void *user_data) {
-  // If the key was held down and now is released then switch off the layer
-  if (grav_tap_state.state == SINGLE_HOLD) {
-    layer_off(_FUNCTION_LAYER);
-  }
-  if (grav_tap_state.state == DOUBLE_HOLD) {
-    layer_off(_NUMPAD_LAYER);
-  }
-  grav_tap_state.state = 0;
-}
-
-// Associate our tap dance key with its functionality
-tap_dance_action_t tap_dance_actions[] = {
-  [ESC_TAP_DANCE] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, esc_layer_finished, esc_layer_reset),
-  [GRV_TAP_DANCE] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, grave_layer_finished, grave_layer_reset)
+const uint16_t PROGMEM grave_combo[] = {LT(_SYMBOLS_LAYER, KC_I), KC_TAB, COMBO_END};
+const uint16_t PROGMEM tilt_combo[] = {MT(MOD_RSFT, KC_L), LT(_SYMBOLS_LAYER, KC_I), KC_TAB, COMBO_END};
+combo_t key_combos[] = {
+    COMBO(grave_combo, KC_GRV),
+    COMBO(tilt_combo, S(KC_GRV)),
 };
 
 uint16_t space_tapping_term = 150;
